62bae0584789992077c0362ab00bda51692e9dea
Mihaszki <pawelmichalski9@gmail.com>
2021-02-16 17:39:50 +0100

    Add server info in info command, Add mongoose package

--- commands/info.js
+++ commands/info.js
@@ -7,7 +7,7 @@ module.exports = {
     const Discord = require('discord.js');
     const config = require('../bot_config.json');
     const loadingMessage = await message.channel.send(':wave:');
-
+    const currentServer = message.client.guilds.cache.find(id => id == message.guild.id);
     const embed = new Discord.MessageEmbed()
       .setColor('#00aeff')
       .setTitle(`${config.bot_name} | Info`)
@@ -16,6 +16,10 @@ module.exports = {
         { name: 'Tag', value: loadingMessage.author.tag, inline: true },
         { name: 'ID', value: loadingMessage.author.id, inline: true },
         { name: 'Author', value: config.bot_author, inline: true },
+        { name: '\u200B', value: '\u200B' },
+        { name: 'Serving on', value: `${message.client.guilds.cache.size} servers`, inline: true },
+        { name: 'Current server', value: currentServer.name, inline: true },
+        { name: 'Region', value: currentServer.region, inline: true },
         { name: '\u200B', value: '\u200B' },
         { name: 'Github', value: config.github_link, inline: true },
         { name: 'Ping', value: `${loadingMessage.createdTimestamp - message.createdTimestamp}ms`, inline: true },
