263c3af9f660863fed0c0a9cb1aaed75a8354963
Mihaszki <pawelmichalski9@gmail.com>
2021-05-26 18:31:15 +0200

    Add guild image to info command

--- commands/info.js
+++ commands/info.js
@@ -15,6 +15,7 @@ module.exports = {
       .setDescription(':heart: **Special thanks to: Jojczak#7487** :heart:')
       .setAuthor(loadingMessage.author.username, message.client.user.avatarURL({ format: 'png', dynamic: true, size: 1024 }))
       .setThumbnail(message.guild.iconURL())
+      .setImage(message.guild.iconURL({ size: 1024 }))
       .addFields(
         { name: 'Tag', value: loadingMessage.author.tag, inline: true },
         { name: 'ID', value: loadingMessage.author.id, inline: true },
@@ -22,7 +23,7 @@ module.exports = {
         { name: 'Serving on', value: `${message.client.guilds.cache.size} servers`, inline: true },
         { name: 'Current server', value: message.guild.name, inline: true },
         { name: 'Region', value: message.guild.region, inline: true },
-        { name: 'Server icon', value: message.guild.iconURL(), inline: true },
+        { name: 'Server icon', value: message.guild.iconURL({ size: 1024, dynamic: true }), inline: true },
         { name: 'Last restart', value: message.client.botRunningUptime.toLocaleString(config.date_locale), inline: true },
         { name: 'Uptime', value: `${uptime} minutes`, inline: true },
         { name: 'Github', value: config.github_link, inline: true },
