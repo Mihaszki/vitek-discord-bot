f4a9017f54262d087b8eae6d6b5f49d15897bd4d
Mihaszki <pawelmichalski9@gmail.com>
2021-08-06 18:05:14 +0200

    Change info command to support the new interactions

--- commands/info.js
+++ commands/info.js
@@ -3,34 +3,24 @@ module.exports = {
   cooldown: 1,
   description: 'Information about the bot',
   guildOnly: true,
-  async execute(message) {
+  async execute(interaction) {
     const Discord = require('discord.js');
     const config = require('../bot_config');
-    const { differenceInMinutes } = require('date-fns');
-    const loadingMessage = await message.channel.send(':wave:');
-    const uptime = differenceInMinutes(new Date(), message.client.botRunningUptime);
+    console.log(interaction.guild.memberCount);
     const embed = new Discord.MessageEmbed()
       .setColor('#00aeff')
-      .setTitle(`${loadingMessage.author.username} | Info`)
       .setDescription(':heart: **Special thanks to: Jojczak#7487** :heart:')
-      .setAuthor(loadingMessage.author.username, message.client.user.avatarURL({ format: 'png', dynamic: true, size: 1024 }))
-      .setThumbnail(message.guild.iconURL())
-      .setImage(message.guild.iconURL({ size: 1024 }))
+      .setAuthor(interaction.client.user.tag, interaction.client.user.avatarURL({ format: 'png', dynamic: true, size: 1024 }))
+      .setThumbnail(interaction.guild.iconURL())
       .addFields(
-        { name: 'Bot ID', value: loadingMessage.author.id, inline: false },
-        { name: 'Author', value: config.bot_author, inline: false },
-        { name: 'Serving on', value: `${message.client.guilds.cache.size} servers`, inline: false },
-        { name: 'Current server', value: message.guild.name, inline: false },
-        { name: 'Region', value: message.guild.region, inline: false },
-        { name: 'Server icon', value: message.guild.iconURL({ size: 1024, dynamic: true }), inline: false },
-        { name: 'Last restart', value: message.client.botRunningUptime.toLocaleString(config.date_locale), inline: false },
-        { name: 'Uptime', value: `${uptime} minutes`, inline: false },
-        { name: 'Github', value: config.github_link, inline: false },
-        { name: 'Ping', value: `${loadingMessage.createdTimestamp - message.createdTimestamp}ms`, inline: false },
-        { name: 'API', value: `${Math.round(message.client.ws.ping)}ms`, inline: false },
+        { name: 'Author', value: config.bot_author },
+        { name: 'Current server', value: interaction.guild.name },
+        { name: 'Total members', value: interaction.guild.memberCount.toString() },
+        { name: 'Server count', value: interaction.client.guilds.cache.size.toString() },
+        { name: 'Server icon', value: interaction.guild.iconURL({ size: 1024, dynamic: true }) },
+        { name: 'Github', value: config.github_link },
       );
 
-    loadingMessage.delete({ timeout: 1000 });
-    message.channel.send(embed);
+    return interaction.reply({ embeds: [embed] });
   },
 };
