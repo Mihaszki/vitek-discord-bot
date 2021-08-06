f9a019ad370dcd476a06b57550260ccdc413aaa3
Mihaszki <pawelmichalski9@gmail.com>
2021-08-06 19:36:18 +0200

    Fix error handling in getImage, Update ascii command, Minor fixes

--- commands/big.js
+++ commands/big.js
@@ -1,21 +1,31 @@
 module.exports = {
   name: 'big',
   description: 'Stretch the image to a resolution of 2137x2137',
-  usage: '<Server emoji/@User/URL>',
+  options: [
+    {
+      name: 'image',
+      description: '@User or Server emoji or URL',
+      type: 'STRING',
+      required: true,
+    },
+  ],
   cooldown: 1,
-  args: true,
-  guildOnly: true,
-  execute(message, args) {
-    const Discord = require('discord.js');
+  async execute(interaction) {
+    const { MessageAttachment } = require('discord.js');
     const getImage = require('../vitek_modules/getImage');
     const Canvas = require('canvas');
+    const img = interaction.options.getString('image');
     const canvas = Canvas.createCanvas(2137, 2137);
     const ctx = canvas.getContext('2d');
-    getImage.getImageAndCheckSize(args[0], message, async (user_image_url) => {
-      const user_image = await Canvas.loadImage(user_image_url);
+    await interaction.reply({ content: 'Generating... :hourglass_flowing_sand:' });
+    getImage.getImageAndCheckSize(img, interaction, async ({ error, url }) => {
+      if(error) {
+        return interaction.editReply({ content: error });
+      }
+      const user_image = await Canvas.loadImage(url);
       ctx.drawImage(user_image, 0, 0, canvas.width, canvas.height);
-      const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'big.png');
-      message.channel.send(attachment);
+      const attachment = new MessageAttachment(canvas.toBuffer(), 'big.png');
+      interaction.editReply({ content: 'Done! :hourglass:', files: [attachment] });
     });
   },
 };
