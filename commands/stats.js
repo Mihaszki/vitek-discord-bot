8106bac7c58ff5b8d44cf47acf6cdbaff8be5ea2
Mihaszki <pawelmichalski9@gmail.com>
2021-08-07 00:27:59 +0200

    Update stats command

--- commands/stats.js
+++ commands/stats.js
@@ -2,31 +2,30 @@ module.exports = {
   name: 'stats',
   description: 'Number of logged messages',
   cooldown: 3,
-  guildOnly: true,
-  async execute(message) {
+  async execute(interaction) {
     const Discord = require('discord.js');
     const messageLogger = require('../vitek_db/messageLogger');
     const getMention = require('../vitek_modules/getMention');
-    messageLogger.count(message, (allMessages, messagesNoBots, userRanking, channelRanking) => {
+    messageLogger.count(interaction, (allMessages, messagesNoBots, userRanking, channelRanking) => {
       let description = '**Most active users:**\n`Place | User | Messages`\n';
       for(let i = 0; i < userRanking.length; i++) {
-        const member = getMention.member(`<@${userRanking[i]._id.user_id}>`, message);
+        const member = getMention.member_interaction(`<@${userRanking[i]._id.user_id}>`, interaction);
         description += `**${i + 1}.** ${member ? member : userRanking[i].username} | ${userRanking[i].count}\n`;
       }
 
       description += '\n**Most active channels:**\n`Place | Channel | Messages`\n';
       for(let i = 0; i < channelRanking.length; i++) {
-        const channel = message.guild.channels.cache.get(channelRanking[i]._id.channel_id);
+        const channel = interaction.guild.channels.cache.get(channelRanking[i]._id.channel_id);
         description += `**${i + 1}.** ${!channel ? channelRanking[i].channel_name : '<#' + channelRanking[i]._id.channel_id + '>'} | ${channelRanking[i].count}\n`;
       }
 
       description += `\n\`\`\`Messages: ${messagesNoBots}\nMessages (+bots): ${allMessages.count}\nWords: ${allMessages.words}\nSwears: ${allMessages.swears}\`\`\``;
       const embed = new Discord.MessageEmbed()
         .setColor('#fc9803')
-        .setTitle(`Logged messages - ${message.guild.name}`)
-        .setThumbnail(message.guild.iconURL())
+        .setTitle(`Logged messages - ${interaction.guild.name}`)
+        .setThumbnail(interaction.guild.iconURL())
         .setDescription(description);
-      message.channel.send(embed);
+      interaction.reply({ embeds: [embed] });
     });
   },
 };
