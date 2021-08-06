c40f768a250d79579ee2d279d37d1adff0b836bd
Mihaszki <pawelmichalski9@gmail.com>
2021-08-06 20:00:49 +0200

    Update coelho and friendship commands to support interactions

--- commands/coelho.js
+++ commands/coelho.js
@@ -1,25 +1,30 @@
 module.exports = {
   name: 'coelho',
-  description: 'Paulo Coelho quotes generator',
-  usage: '<text>',
+  description: 'Paulo Coelho quote generator',
   cooldown: 3,
-  args: true,
-  guildOnly: true,
-  async execute(message) {
+  options: [
+    {
+      name: 'text',
+      description: 'Enter a quote',
+      type: 'STRING',
+      required: true,
+    },
+  ],
+  async execute(interaction) {
     const Discord = require('discord.js');
     const Canvas = require('canvas');
     const canvas = Canvas.createCanvas(1280, 720);
     const context = canvas.getContext('2d');
-    const { prefix } = require('../bot_config');
     const canvasDraw = require('../vitek_modules/canvasDraw');
+    await interaction.reply({ content: 'Generating... :hourglass_flowing_sand:' });
 
-    const msg = message.cleanContent.slice(prefix.length + this.name.length + 1);
+    const msg = interaction.options.getString('text');
     const background = await Canvas.loadImage('images/coelho/coelho.png');
     context.drawImage(background, 0, 0, canvas.width, canvas.height);
     canvasDraw.wrapText(context, `„${msg}”`, {
       x: 475, maxWidth: 800, fontColor: '#FFFFFF', quoteAuthor: '~ Paulo Coelho',
     });
     const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'coelho.png');
-    message.channel.send(attachment);
+    interaction.editReply({ content: 'Done! :hourglass:', files: [attachment] });
   },
 };
--- commands/friendship.js
+++ commands/friendship.js
@@ -1,25 +1,48 @@
 module.exports = {
   name: 'friendship',
   description: 'Friendship ended with X, now Y is my best friend',
-  usage: '<Word1> <Word2> <Server emoji/@User/URL>',
   cooldown: 1,
-  args: true,
-  guildOnly: true,
-  execute(message, args) {
-    const Discord = require('discord.js');
-    const { prefix } = require('../bot_config');
+  options: [
+    {
+      name: 'word1',
+      description: 'Enter a first word',
+      type: 'STRING',
+      required: true,
+    },
+    {
+      name: 'word2',
+      description: 'Enter a second word',
+      type: 'STRING',
+      required: true,
+    },
+    {
+      name: 'image',
+      description: '@User or Server emoji or URL',
+      type: 'STRING',
+      required: true,
+    },
+  ],
+  async execute(interaction) {
+    const { MessageAttachment } = require('discord.js');
     const getImage = require('../vitek_modules/getImage');
     const Canvas = require('canvas');
     const canvas = Canvas.createCanvas(500, 372);
     const context = canvas.getContext('2d');
     const { drawTextInBox } = require('../vitek_modules/canvasDraw');
 
-    if(!args[0] || !args[1] || !args[2]) return message.channel.send(`You must give all three arguments!\n\`${prefix}${this.name} ${this.usage}\``);
+    const img = interaction.options.getString('image');
+    const word1 = interaction.options.getString('word1').trim().split(' ')[0];
+    const word2 = interaction.options.getString('word2').trim().split(' ')[0];
 
-    getImage.getImageAndCheckSize(args[2], message, async (user_image_url) => {
+    await interaction.reply({ content: 'Generating... :hourglass_flowing_sand:' });
+
+    getImage.getImageAndCheckSize(img, interaction, async ({ error, url }) => {
+      if(error) {
+        return interaction.editReply({ content: error });
+      }
       const background = await Canvas.loadImage('images/friendship/friendship.png');
       const foreground = await Canvas.loadImage('images/friendship/x.png');
-      const user_image = await Canvas.loadImage(user_image_url);
+      const user_image = await Canvas.loadImage(url);
       context.drawImage(background, 0, 0, canvas.width, canvas.height);
       context.drawImage(user_image, 0, 207, 106, 165);
       context.drawImage(user_image, 361, 225, 139, 147);
@@ -40,7 +63,7 @@ module.exports = {
       context.font = '30px sans-serif';
       context.fillStyle = gradient;
 
-      drawTextInBox(context, args[0], 'sans-serif', 328, -5, 160, 51);
+      drawTextInBox(context, word1, 'sans-serif', 328, -5, 160, 51);
 
       const gradient2 = context.createLinearGradient(0, 0, canvas.width, 0);
       gradient2.addColorStop(0, '#bd5516');
@@ -50,10 +73,10 @@ module.exports = {
       gradient2.addColorStop(1, '#bd5516');
       context.fillStyle = gradient2;
 
-      drawTextInBox(context, args[1], 'sans-serif', 195, 85, 110, 50);
+      drawTextInBox(context, word2, 'sans-serif', 195, 85, 110, 50);
 
-      const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'friendship.png');
-      message.channel.send(attachment);
+      const attachment = new MessageAttachment(canvas.toBuffer(), 'friendship.png');
+      interaction.editReply({ content: 'Done! :hourglass:', files: [attachment] });
     });
   },
 };
