ee5274d6ac5d6f131060c1fa19e95b64f48997e3
Mihaszki <pawelmichalski9@gmail.com>
2022-03-22 18:02:33 +0100

    Update ascii, catjam, cmm commands

--- commands/catjam.js
+++ commands/catjam.js
@@ -1,15 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
 module.exports = {
-  name: 'catjam',
-  description: 'catJAM animation',
-  options: [
-    {
-      name: 'image',
-      description: '@User or Server emoji or URL',
-      type: 'STRING',
-      required: true,
-    },
-  ],
-  cooldown: 5,
+  data: new SlashCommandBuilder()
+    .setName('catjam')
+    .setDescription('catJAM animation')
+    .addStringOption(option =>
+      option.setName('image')
+        .setDescription('@User or Server emoji or URL')
+        .setRequired(true)),
   async execute(interaction) {
     const { MessageAttachment } = require('discord.js');
     const getImage = require('../vitek_modules/getImage');
@@ -20,7 +18,9 @@ module.exports = {
     const encoder = new GIFEncoder(350, 190);
     ctx.fillStyle = '#000000';
     const img = interaction.options.getString('image');
-    await interaction.reply({ content: 'Generating... :hourglass_flowing_sand:' });
+
+    await interaction.deferReply();
+
     getImage.getImageAndCheckSize(img, interaction, async ({ error, url }) => {
       if(error) {
         return interaction.editReply({ content: error });
@@ -41,7 +41,7 @@ module.exports = {
       encoder.finish();
       const buffer = encoder.out.getData();
       const attachment = new MessageAttachment(buffer, 'catjam.gif');
-      interaction.editReply({ content: 'Done! :hourglass:', files: [attachment] });
+      interaction.editReply({ files: [attachment] });
     });
   },
 };
